fff68f8d13eb01f3011a02a50ef6d02ca19cd24a
András Sólyom <37068759+solyoma@users.noreply.github.com>
2026-06-04 21:43:16 +0200

    Translation changes version to 1.1.1

--- src/version.h
+++ src/version.h
@@ -6,6 +6,6 @@ constexpr const char* FalconCalc_Hist_File = "FalconCalc.hist";
 constexpr const char* FalconCalc_Dat_File = "FalconCalc.dat";
 constexpr const char* FalconCalc_State_File = "FalconCalc.cfg";
 
-constexpr const int   VERSION_INT = 0x010100;
-constexpr const char* VERSION_STRING = "1.1.0";
+constexpr const int   VERSION_INT = 0x010101;
+constexpr const char* VERSION_STRING = "1.1.1";
 
