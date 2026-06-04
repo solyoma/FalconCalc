dff60a393b35f2cccde7a9625d875545c0434fa1
András Sólyom <37068759+solyoma@users.noreply.github.com>
2026-06-04 11:18:09 +0200

    project changes, explicitely added version.h from FalconCalc (non-QT) and changed version to 1.1.0

--- src/version.h
+++ src/version.h
@@ -1,4 +1,4 @@
-#pragma once
+﻿#pragma once
 
 constexpr const char* STATE_ID_STRING = "FalconCalc State File V";
 constexpr const char* DAT_ID_STRING = "FalconCalc Data File V";
@@ -6,6 +6,6 @@ constexpr const char* FalconCalc_Hist_File = "FalconCalc.hist";
 constexpr const char* FalconCalc_Dat_File = "FalconCalc.dat";
 constexpr const char* FalconCalc_State_File = "FalconCalc.cfg";
 
-constexpr const int   VERSION_INT = 0x010000;
-constexpr const char* VERSION_STRING = "1.0.0";
+constexpr const int   VERSION_INT = 0x010100;
+constexpr const char* VERSION_STRING = "1.1.0";
 
